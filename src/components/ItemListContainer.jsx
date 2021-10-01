5366c468e142cda7f8c336432a226570373f1c0a
Carlos Ruiz <caosrupe@gmail.com>
2021-09-30 22:33:06 -0300

    adicion de style

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export default function ItemListContainer({saludo}) {
+    
+   const divStyles = {color: "#60269e", FontSize:"20px"} 
     return (
-        <>
+        <div style={divStyles}>
             <h1>{saludo}</h1>
-        </>
+        </div>
     )
 }
